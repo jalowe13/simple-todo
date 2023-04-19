b25efbdc482d865d243230c24f704931e401948d
Jacob Lowe <jplowe21@vt.edu>
2023-04-19 14:29:45 -0400

    0.1 - Created Task Line Creation
    - Needs to be moved to its own method
    - Experimental buttons added

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,17 @@
+// Jacob Lowe
 namespace todolist;
+
 
 public partial class Form1 : Form
 {
+    int task_y = 150;       // Current y
+    int task_max_y = 400;   // Maximum y for creating tasks
     public Form1()
     {
         InitializeComponent();
+
+
+        // Header Textbox
         RichTextBox tb_entry = new RichTextBox();
         tb_entry.Location = new Point(10,10);
         tb_entry.Size = new Size(700,100);
@@ -17,6 +24,71 @@ public partial class Form1 : Form
         tb_entry.ReadOnly = true;
         tb_entry.Enabled = false;
 
+
+        // Create Task Button
+        Button create_task = new Button();
+        create_task.Location = new Point(10,task_y-20);
+        create_task.Text = "Add";
+        create_task.Click += new EventHandler(create_task_Click);
+        this.Controls.Add(create_task);
+
     }
+    // Check if it is apropriate to create a new task line
+    public bool check_task_max()
+    {
+        if(task_y < task_max_y)
+        {
+            task_y += 20;
+            return true;
+        }
+        return false;
+    }
+    // Click action for creating a new task
+    private void create_task_Click(object? sender, EventArgs? e)
+    {
+        // Get the button that was clicked.
+        if (sender != null)
+        {
+            Button button = (Button)sender;
+
+            // Display a message box.
+            if (check_task_max())
+            {
+                // Taskbox
+                RichTextBox new_task = new RichTextBox();
+                new_task.Location = new Point(10,task_y);
+                new_task.Size = new Size(700,20);
+                this.Controls.Add(new_task);
+                // Complete and clear buttons
+                Button complete = new Button();
+                Button clear = new Button();
+                complete.Location = new Point(710,task_y);
+                clear.Location = new Point(730,task_y);
+                complete.Size = new Size(20,20);
+                clear.Size = new Size(20,20);
+                complete.Text = "O";
+                clear.Text = "X";
+
+                complete.Click += new EventHandler(complete_Click);
+                clear.Click += new EventHandler(clear_Click);
+                this.Controls.Add(complete);
+                this.Controls.Add(clear);
+            }
+        }
+    }
+
+    // Remove textbox on call
+    private void complete_Click(object? sender, EventArgs? e)
+    {
+        MessageBox.Show("You clicked the complete button.");
+    }
+
+    // Clear textbox on call
+    private void clear_Click(object? sender, EventArgs? e)
+    {
+        MessageBox.Show("You clicked the clear button.");
+    }
+
+
     
 }
